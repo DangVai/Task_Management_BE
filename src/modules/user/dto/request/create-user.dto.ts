e03a86405fb20220608fb143220d449e41f803bc
unknown <xomdangvaisf@gmail.com>
2025-12-26 15:55:16 +0700

    add new validate

--- src/modules/user/dto/request/create-user.dto.ts
+++ src/modules/user/dto/request/create-user.dto.ts
@@ -1,19 +1,31 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength, Matches } from 'class-validator';
 
 export class CreateUserDto {
     @IsEmail()
     @IsNotEmpty()
-    email: string;
+    @Matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+        message: "Invalid email format",
+    })
+    email!: string;
 
     @IsString()
     @IsNotEmpty()
-    username: string;
+    @MinLength(3)
+    @Matches(/^[a-zA-Z0-9_]+$/, {
+        message: "Username can only contain letters, numbers, and underscores", 
+    })
+    username!: string;
 
     @IsString()
     @MinLength(6)
-    password: string;
+    @IsNotEmpty()
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, {
+      message: "Password must contain at least one uppercase letter, one lowercase letter, and one number",
+    })
+    password!: string;
 
     @IsString()
     @IsOptional()
+    @MinLength(2)
     fullName?: string;
 }
